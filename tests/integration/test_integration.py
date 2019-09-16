114948e67038bfda83a5033657a44f9f1d8e33bc
naselin <nikita@selin.com.ru>
2019-09-16 12:47:18 +0300

    Added *disconnected_store* tests.

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,20 +3,20 @@
 
 import hashlib
 import datetime
-import functools
 import unittest
 
 from api import api, store
 from tests.cases import cases
 
+
 class TestSuite(unittest.TestCase):
     def setUp(self):
         self.context = {}
         self.headers = {}
-        self.settings = store.Store()
+        self.store = store.Store(log=False)
 
     def get_response(self, request):
-        return api.method_handler({"body": request, "headers": self.headers}, self.context, self.settings)
+        return api.method_handler({"body": request, "headers": self.headers}, self.context, self.store)
 
     def set_valid_auth(self, request):
         if request.get("login") == api.ADMIN_LOGIN:
@@ -128,6 +128,48 @@ class TestSuite(unittest.TestCase):
         self.assertTrue(all(v and isinstance(v, list) and all(isinstance(i, basestring) for i in v)
                         for v in response.values()))
         self.assertEqual(self.context.get("nclients"), len(arguments["client_ids"]))
+
+    @cases([
+        {"client_ids": [1, 2, 3], "date": datetime.datetime.today().strftime("%d.%m.%Y")},
+        {"client_ids": [1, 2], "date": "19.07.2017"},
+        {"client_ids": [0]},
+    ])
+    def test_disconnected_store_interests_request(self, arguments):
+        request = {"account": "horns&hoofs", "login": "h&f", "method": "clients_interests", "arguments": arguments}
+        self.set_valid_auth(request)
+        self.store.disconnect()
+        response, code = self.get_response(request)
+        self.assertEqual(api.INTERNAL_ERROR, code)
+
+    @cases([
+        {"phone": "79175002040", "email": "stupnikov@otus.ru"},
+        {"phone": 79175002040, "email": "stupnikov@otus.ru"},
+        {"gender": 1, "birthday": "01.01.2000", "first_name": "a", "last_name": "b"},
+        {"gender": 0, "birthday": "01.01.2000"},
+        {"gender": 2, "birthday": "01.01.2000"},
+        {"first_name": "a", "last_name": "b"},
+        {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000",
+         "first_name": "a", "last_name": "b"},
+    ])
+    def test_disconnected_store_score_request(self, arguments):
+        request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
+        self.set_valid_auth(request)
+        self.store.disconnect()
+        response, code = self.get_response(request)
+        self.assertEqual(api.OK, code, arguments)
+        score = response.get("score")
+        self.assertTrue(isinstance(score, (int, float)) and score >= 0, arguments)
+        self.assertEqual(sorted(self.context["has"]), sorted(arguments.keys()))
+
+    def test_disconnected_store_admin_request(self):
+        arguments = {"phone": "79175002040", "email": "stupnikov@otus.ru"}
+        request = {"account": "horns&hoofs", "login": "admin", "method": "online_score", "arguments": arguments}
+        self.set_valid_auth(request)
+        self.store.disconnect()
+        response, code = self.get_response(request)
+        self.assertEqual(api.OK, code)
+        score = response.get("score")
+        self.assertEqual(score, 42)
 
 
 if __name__ == "__main__":
